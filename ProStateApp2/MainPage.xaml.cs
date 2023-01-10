194d04b79a810802126301c9c3d40adadf078e86
Armands Liepa <armandsliepa3@gmail.com>
2023-01-10 21:15:50 +0200

    login lapas validācija laikam pabeigta

--- ProStateApp2/MainPage.xaml.cs
+++ ProStateApp2/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿namespace ProStateApp2;
 using MySql.Data.MySqlClient;
+using System.Text.RegularExpressions;
 using static ProStateApp2.App;
 
 public partial class MainPage : ContentPage
@@ -21,9 +22,40 @@ public partial class MainPage : ContentPage
 
             string inputEmail = EpastsEntry.Text;
             string inputPassword = ParoleEntry.Text;
+
+            var emailRegex = new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+
+            // Pārbauda vai vispār kaut kas ir ievadīts inputā
+            if (string.IsNullOrEmpty(inputEmail) || string.IsNullOrEmpty(inputPassword))
+            {
+                inputError.IsVisible = true;
+                emailError.IsVisible = false;
+                Error.IsVisible = false;
+                return;
+            }
+            else
+            {
+                inputError.IsVisible = false;
+            }
+
+            // Pārbauda vai epasts ir derīgs
+            if (!emailRegex.IsMatch(inputEmail))
+            {
+                emailError.IsVisible = true;
+                Error.IsVisible = false;
+                return;
+            }
+            else
+            {
+                emailError.IsVisible = false;
+            }
             using (var command = new MySqlCommand($"SELECT * FROM user WHERE email = \"{inputEmail}\"", connection))
             using (var reader = command.ExecuteReader())
             {
+                if (!reader.HasRows)
+                {
+                    Error.IsVisible = true;
+                }
                 while (reader.Read())
                 {
                     string password = reader.GetString(4);
