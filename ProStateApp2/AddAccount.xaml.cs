4e0e41a435b261cda90c937f4fea68139881e6c0
Armands Liepa <armandsliepa3@gmail.com>
2023-01-22 21:43:18 +0200

    Konta pievienošana ir!!

--- ProStateApp2/AddAccount.xaml.cs
+++ ProStateApp2/AddAccount.xaml.cs
@@ -1,3 +1,6 @@
+using MySql.Data.MySqlClient;
+using static ProStateApp2.App;
+
 namespace ProStateApp2;
 
 public partial class AddAccount : ContentPage
@@ -6,4 +9,47 @@ public partial class AddAccount : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private string GenerateAccountNumber()
+    {
+        var random = new Random();
+        var firstTwo = random.Next(10, 99);
+        var nextEight = random.Next(10000000, 999999999);
+        var lastFive = random.Next(10000, 99999);
+        var generatedAccountNumber = "LV" + firstTwo + "PRST" + nextEight + lastFive;
+        return generatedAccountNumber;
+    }
+
+    private void Account_Created(object sender, EventArgs e)
+    {
+        var accountNumber = GenerateAccountNumber();
+        var accountName = accountNameEntry.Text;
+        var dailyLimit = decimal.Parse(dailyLimitEntry.Text);
+        var monthlyLimit = decimal.Parse(monthlyLimitEntry.Text);
+        var userId = GlobalVariables.CurrentUser.id;
+
+        string connectionString = "server=192.168.1.198;user=armands;database=pro_state;password=qwerty;";
+        using (var connection = new MySqlConnection(connectionString))
+        {
+            connection.Open();
+
+            var command = new MySqlCommand("INSERT INTO account (number, name, user_id, available, reserved, daily_limit, monthly_limit) VALUES (@accountNumber, @accountName, @userId, 0, 0, @dailyLimit, @monthlyLimit)", connection);
+            command.Parameters.AddWithValue("@accountNumber", accountNumber);
+            command.Parameters.AddWithValue("@accountName", accountName);
+            command.Parameters.AddWithValue("@userId", userId);
+            command.Parameters.AddWithValue("@dailyLimit", dailyLimit);
+            command.Parameters.AddWithValue("@monthlyLimit", monthlyLimit);
+
+            try
+            {
+                command.ExecuteNonQuery();
+                DisplayAlert("Success", "Account added successfully", "OK");
+                Navigation.PopAsync();
+            }
+            catch (MySqlException ex)
+            {
+                DisplayAlert("Error", ex.ToString(), "OK");
+            }
+        }
+    }
 }
